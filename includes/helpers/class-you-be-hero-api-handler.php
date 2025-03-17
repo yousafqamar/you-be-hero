b9d29b0831cdc99d6fe3d27a07d306d352f6c0c6
Muhammad Iqbal <malikiqbal849@gmail.com>
2025-03-17 17:53:11 +0500

    update api helper class
    add mock json response

--- includes/helpers/class-you-be-hero-api-handler.php
+++ includes/helpers/class-you-be-hero-api-handler.php
@@ -17,6 +17,8 @@ class YouBeHero_API_Handler {
 
     private $api_base_url;
     private $bearer_token;
+    private $mock = 'yes';
+    private $api_json;
 
     /**
      * Constructor - Initializes API URL and Token.
@@ -25,6 +27,8 @@ class YouBeHero_API_Handler {
 
         $this->api_base_url = 'https://your-api-url.com/v1/'; // Change this to your actual API URL
         $this->bearer_token = get_option( 'youbehero_api_token' ); // Fetch stored token from DB
+
+        $this->api_json = $this->mock == 'yes' ? $this->ybh_get_predefined_json() : $this->get( 'fix_price' );
 
     }
 
@@ -116,4 +120,180 @@ class YouBeHero_API_Handler {
         return $decoded_response;
 
     }
+
+    /**
+     * @return string
+     */
+    private function ybh_get_predefined_json() {
+
+        // dummy response json here
+        return '{
+            "success": true,
+            "data": {
+                "shop_id": 2523,
+                "eshop_logo": null,
+                "company_name": "fern.com",
+                "url": "https:\/\/fern.com",
+                "status": "acitve",
+                "total_credits": 50,
+                "payment_count": 1,
+                "selected_causes": [{
+                    "id": 264,
+                    "name": "50 και Ελλάς",
+                    "url": "https:\/\/youbehero.com\/gr\/cause\/50kai-ellas",
+                    "image": "https:\/\/youbehero.com\/images\/cause\/264\/l\/50kai_logo.png",
+                    "website": "https:\/\/www.50plus.gr\/",
+                    "social_links": {
+                        "facebook": "https:\/\/www.facebook.com\/50plushellas",
+                        "twitter": "https:\/\/twitter.com\/50plusHellas",
+                        "youtube": "https:\/\/www.youtube.com\/channel\/UCv2Yj96ri3GLMcQVU3i5CTg",
+                        "linkedin": null,
+                        "google": null,
+                        "instagram": "https:\/\/www.instagram.com\/50plushellas\/",
+                        "tumblr": null,
+                        "pinterest": null
+                    }
+                }, {
+                    "id": 120,
+                    "name": "APICCO -Community Based Art",
+                    "url": "https:\/\/youbehero.com\/gr\/cause\/apicco-community-based-art",
+                    "image": "https:\/\/youbehero.com\/images\/cause\/120\/l\/Apicco-logo.jpeg",
+                    "website": "http:\/\/www.apicco.net\/",
+                    "social_links": {
+                        "facebook": "https:\/\/www.facebook.com\/ApiccoCommunity",
+                        "twitter": null,
+                        "youtube": "https:\/\/www.youtube.com\/channel\/UCHZRa09HqC8e1xqPmYwGlGg?view_as=subscriber",
+                        "linkedin": "https:\/\/www.linkedin.com\/company\/apicco-community-based-art\/",
+                        "google": null,
+                        "instagram": "https:\/\/www.instagram.com\/apicco_prison_project\/",
+                        "tumblr": null,
+                        "pinterest": null
+                    }
+                }, {
+                    "id": 330,
+                    "name": "Σύλλογος Αγία Σοφία",
+                    "url": "https:\/\/youbehero.com\/gr\/cause\/sillogos-goneon-kai-kidemonon-paidion-me-siggenis-kardiopathies-i-agia-sofia",
+                    "image": "https:\/\/youbehero.com\/images\/cause\/330\/l\/agia-sofia-logo.png",
+                    "website": null,
+                    "social_links": {
+                        "facebook": "https:\/\/www.facebook.com\/profile.php?id=61555629857310",
+                        "twitter": "https:\/\/twitter.com\/pedocardio?lang=el",
+                        "youtube": null,
+                        "linkedin": null,
+                        "google": null,
+                        "instagram": null,
+                        "tumblr": null,
+                        "pinterest": null
+                    }
+                }, {
+                    "id": 17,
+                    "name": "Look to the Stars",
+                    "url": "https:\/\/youbehero.com\/gr\/cause\/looktothestars",
+                    "image": "https:\/\/youbehero.com\/images\/cause\/17\/l\/looktothestars_logo.jpeg",
+                    "website": "https:\/\/looktothestars.gr\/",
+                    "social_links": {
+                        "facebook": "https:\/\/www.facebook.com\/looktothestars7\/",
+                        "twitter": "https:\/\/twitter.com\/looktothestars9\/",
+                        "youtube": "https:\/\/www.youtube.com\/channel\/UCgUrlps4qRt9U2bGv40-gaQ\/",
+                        "linkedin": null,
+                        "google": "https:\/\/plus.google.com\/u\/0\/112749431543258923190",
+                        "instagram": "https:\/\/www.instagram.com\/looktothestars7\/",
+                        "tumblr": null,
+                        "pinterest": null
+                    }
+                }],
+                "donation_schedule": "{\"start\":{\"date\":\"2025-01-17\",\"time\":\"12:00\"},\"end\":{\"endDate\":null,\"endTime\":null}}",
+                "donation_settings": {
+                    "donor": "customer", /*or eshop*/
+                    "donationType": "fixed", /*or roundup, or percentage*/
+                    "fixed_amounts": {
+                        "A": "0,50",
+                        "B": "1,00",
+                        "C": "2,00"
+                    },
+                    "fixed_amount": null,
+                    "fixedPercentage": "25.00"
+                },
+                "widget_configurations": {
+                    "product_page": {
+                        "product_page": {
+                            "active": true,
+                            "theme": "discreet",
+                            "position": "below",
+                            "background_color": "#f4f4f4",
+                            "text_color": "#2d2d2d",
+                            "display": "analytical",
+                            "icon": "generic",
+                            "border": false,
+                            "border_color": "#040706",
+                            "border_radius": "bigBorderRadius",
+                            "margin": "smallMargin",
+                            "padding": "smallPadding"
+                        }
+                    },
+                    "checkout_page": {
+                        "checkout_page": {
+                            "theme": "discreet",
+                            "position": "over",
+                            "background_color": "#f7f7f7",
+                            "text_color": "#1b2b23",
+                            "btn_color": "#30942e",
+                            "display": "short",
+                            "border": false,
+                            "border_color": "#b53a6a",
+                            "border_radius": "bigBorderRadius",
+                            "margin": "bigMargin",
+                            "padding": "midPadding"
+                        }
+                    },
+                    "confirmation_page": {
+                        "confirmation_page": {
+                            "active": true,
+                            "theme": "light",
+                            "background_color": "#ffffff",
+                            "plaisio_color": "#f7f7f7",
+                            "text_color": "#212121",
+                            "display": "short",
+                            "border": true,
+                            "border_color": "#e0e0e0",
+                            "border_radius": "bigBorderRadius",
+                            "margin": "bigMargin",
+                            "padding": "midPadding"
+                        }
+                    },
+                    "confirmation_email": {
+                        "confirmation_email": {
+                            "active": true,
+                            "theme": "fancy",
+                            "background_color": "#fefefe",
+                            "text_color": "#424242",
+                            "display": "short",
+                            "border": true,
+                            "border_color": "#e1e1e1",
+                            "border_radius": "midBorderRadius",
+                            "margin": "bigMargin",
+                            "padding": "bigPadding"
+                        }
+                    },
+                    "generic_marketing": {
+                        "generic_marketing": {
+                            "theme": "discreet",
+                            "background_color": "#fefcfc",
+                            "text_color": "#474747",
+                            "display": "short",
+                            "icon": "generic",
+                            "border": false,
+                            "border_color": "#92f8fa",
+                            "border_radius": "bigBorderRadius",
+                            "margin": "smallMargin",
+                            "padding": "smallPadding"
+                        }
+                    }
+                },
+                "eshop_details": null
+            }
+        }';
+
+    }
+
 }
