42851aa42b70259217e5967d344da3f99dea6eca
Muhammad Iqbal <malikiqbal849@gmail.com>
2025-04-03 16:50:29 +0500

    design modification
    update donation box design structure

--- build/render.php
+++ build/render.php
@@ -9,30 +9,14 @@ $amounts = $attributes['amounts'];
         
         <div class="donation-box">
             <h3><?php _e('Make a Donation', 'woocommerce'); ?></h3>
-            <p><?php _e('Select a cause and donation amount:', 'woocommerce'); ?></p>
+<!--            <p>--><?php //_e('Select a cause and donation amount:', 'woocommerce'); ?><!--</p>-->
             <div class="donation-item">
-                <img src="https://via.placeholder.com/40" alt="logo">
-            <span>Save your hood</span>
-                <select id="donation-cause" name="donation_cause">
-                    <option value=""><?php _e('Select a cause', 'woocommerce'); ?></option>
-                    <?php foreach ($causes as $cause) : ?>
-                        <option class="<?php echo $cause['value']; ?>" value="<?php echo $cause['value']; ?>">
-                            <?php echo esc_html($cause['label']); ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
+<!--                <img src="https://via.placeholder.com/40" alt="logo">-->
+<!--                <span>Save your hood</span>-->
+
             </div>
             <div id="donation-amounts" class="donation-buttons">
-                <?php foreach ($amounts as $amount) : ?>
-                    <label class="radio-button">
-                        <input type="radio" name="donation_amount" value="<?php echo esc_attr($amount); ?>" class="visually-hidden">
-                        <?php echo esc_html('$' . $amount); ?>
-                    </label>
-                <?php endforeach; ?>
-                    <label class="radio-button">
-                        <input type="radio" name="donation_amount" value="0" class="visually-hidden">
-                        🗑
-                    </label>
+
             </div>
         </div>
   </div>
--- public/js/you-be-hero-public.js
+++ public/js/you-be-hero-public.js
@@ -36,19 +36,59 @@
         const $causeSelect = $('#donation-cause');
         const $amountsContainer = $('#donation-amounts');
 
-        $causeSelect.html("");
+        $('.donation-item').before(`<div class="donation-box-container">
+                <div class="donation-header">
+                    <span>Θα θέλατε να κάνετε μια δωρεά;</span>
+                    <span class="donation-amount">0,00€</span>
+                </div>
+
+                <!-- Custom Dropdown -->
+                <div class="custom-dropdown">
+                    <div class="donation-select custom-dropdown-toggle" id="customDropdownToggle">
+                        <div class="donation-text">
+                            <img src="https://via.placeholder.com/35" alt="Logo">
+                            <span id="selectedOption">Save your hood</span>
+                        </div>
+                        <span class="dropdown-arrow">▼</span>
+                    </div>
+                    <div class="custom-dropdown-menu" id="dropdownMenu">
+                        <!-- Add more options here if needed -->
+                    </div>
+                </div>
+
+                <!-- Radio Buttons -->
+                <div class="donation-buttons">
+                    <button>0,5€</button>
+                    <button class="active">1€</button>
+                    <button>3€</button>
+                    <button class="delete-button">🗑</button>
+                </div>
+                <input name="donation_cause" id="donation-cause" type="hidden"/>
+                <input name="donation_amount" id="donation-amount" type="hidden"/>
+             </div>`)
+
+
+        $('#dropdownMenu').html("");
         causes.forEach((cause) => {
-            $causeSelect.append(`<option value="${cause.value}">${cause.label}</option>`);
+            $('#dropdownMenu').append(`<div class="custom-dropdown-option" id="customDropdownOption" data-text="${cause.label}" data-value="${cause.value}")">
+                      <img alt="${cause.label}" src="${cause.image}"/>
+                      <span class="text-gray-700">${cause.label}</span>
+                    </div>`);
         });
 
-        amounts.forEach((amount) => {
-            amount = parseInt(amount)
-            $amountsContainer.prepend(`
-                <label class="radio-button">
-                    <input type="radio" name="donation_amount" value="${amount}" class="visually-hidden"> $${amount}
-                </label>
-            `);
-        });
+        // $causeSelect.html("");
+        // causes.forEach((cause) => {
+        //     $causeSelect.append(`<option value="${cause.value}">${cause.label}</option>`);
+        // });
+
+        // amounts.forEach((amount) => {
+        //     amount = parseInt(amount)
+        //     $amountsContainer.prepend(`
+        //         <label class="radio-button">
+        //             <input type="radio" name="donation_amount" value="${amount}" class="visually-hidden"> $${amount}
+        //         </label>
+        //     `);
+        // });
         
         
         function addDonation(donation_amount, donation_cause){
@@ -89,6 +129,109 @@
                 addDonation( donation_amount, donation_cause );
             }
         });
+
+        //=======//=====//
+        function update_donation_to_cart( donation_amount, donation_cause ){
+
+            if (donation_amount && donation_cause) {
+                jQuery.ajax({
+                    type: 'POST',
+                    url: wc_checkout_params.ajax_url,
+                    data: {
+                        action: 'update_donation_fee',
+                        donation_amount: donation_amount,
+                        donation_cause: donation_cause
+                    },
+                    success: function(response) {
+                        jQuery('body').trigger('update_checkout');
+                    }
+                });
+            }
+        }
+        // JavaScript to handle dropdown toggle and selection
+        // function toggleDropdown() {
+
+                // const dropdownMenu = document.getElementById('dropdownMenu');
+                // dropdownMenu.classList.toggle('show');
+
+        document.getElementById("customDropdownToggle").addEventListener("click", function () {
+            document.getElementById("dropdownMenu").classList.toggle("show");
+        });
+
+        // }
+
+        document.getElementById("customDropdownOption").addEventListener("click", function () {
+            console.log('ads')
+        // function selectOption(optionText, value) {
+            const selectedOption = document.getElementById('selectedOption');
+            const donationCauseEle = document.getElementById('donation-cause');
+            selectedOption.textContent = $(this).data("text");//optionText;
+            toggleDropdown(); // Close the dropdown after selection
+            console.log('Selected Value:', $(this).data("value")); // Log the selected value
+            donationCauseEle.value = $(this).data("value");
+        // }
+        });
+
+        // Close the dropdown if clicked outside
+        window.onclick = function(event) {
+            if (!event.target.matches('.custom-dropdown-toggle')) {
+                const dropdowns = document.querySelectorAll('.custom-dropdown-menu');
+                dropdowns.forEach(dropdown => {
+                    if (dropdown.classList.contains('show')) {
+                        dropdown.classList.remove('show');
+                    }
+                });
+            }
+        };
+
+        // JavaScript to handle radio button selection
+        function selectRadioButton(value) {
+            const radioButtons = document.querySelectorAll('.radio-button');
+            radioButtons.forEach(button => {
+                button.classList.remove('selected');
+            });
+            event.target.classList.add('selected');
+
+            const donationAmountEle = document.getElementById('donation-amount');
+            donationAmountEle.value = value;
+            console.log('Selected Radio Value:', value); // Log the selected radio value
+            const donationCauseEle = document.getElementById('donation-cause');
+            let donation_cause = donationCauseEle.value;
+            update_donation_to_cart( value, donation_cause );
+        }
+
+
+        // jQuery(document).ready(function($) {
+            $('#donation-amount').change(function() {
+                var donation_amount = $(this).val();
+                var donation_cause = $('#donation-cause').val();
+                console.log(donation_cause, donation_amount);
+
+                if (donation_amount && donation_cause) {
+                    $.ajax({
+                        type: 'POST',
+                        url: wc_checkout_params.ajax_url,
+                        data: {
+                            action: 'update_donation_fee',
+                            donation_amount: donation_amount,
+                            donation_cause: donation_cause
+                        },
+                        success: function(response) {
+                            $('body').trigger('update_checkout');
+                        }
+                    });
+                }
+            });
+
+            $('#donation-cause').change(function() {
+                console.log($('#donation-cause'));
+                var donation_cause = $(this).val();
+                var donation_amount = $('#donation-amount').val();
+                update_donation_to_cart( donation_amount, donation_cause );
+            });
+
+        // });
+        //====//===//
     });
 
 })( jQuery );
