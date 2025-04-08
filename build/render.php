c137654530239f0fd35f4e431357cde12bbdd913
Muhammad Iqbal <malikiqbal849@gmail.com>
2025-04-08 20:44:26 +0500

    add donation widgets

--- build/render.php
+++ build/render.php
@@ -14,6 +14,60 @@ $causes = array_map(function ($cause) {
 
 $amounts = array_values($youbehero_data['donation_settings']['fixed_amounts']);
 
+/**
+ * =======================
+ * Dummpy Values  - Start
+ * =======================
+ */
+    $donor = 'eshop'; // customer, eshop
+    $donationType = 'percentage'; // fixed, roundup, percentage
+
+    $html = $headHtml = '';
+    if ($donor == 'customer' &&  $donationType == 'fixed') {
+
+            $headHtml .= '<span>Θα θέλατε να κάνετε μια δωρεά;</span><span class="donation-amount-pill">0,00'.$currency_symbol.'</span>';
+
+            foreach ($amounts as $amount) {
+                $amount_cents = (int)$amount * 100;
+
+                $html .= '<button class="radio-button" data-value="'.$amount_cents.'" data-label="'.$amount.'" >'.$amount.'</button>';
+
+            }
+            $html .= '<button class="delete-button">🗑</button>';
+
+    } else if ($donor == 'customer' &&  $donationType == 'roundup') {
+
+        $headHtml .= '<span>Θα θέλατε να κάνετε μια δωρεά;</span><span class="donation-amount-pill">0,00'.$currency_symbol.'</span>';
+
+        $html .= '<button class="radio-button" data-value="0.80" data-label="0.80" >0.80</button>';
+        $html .= '<button class="delete-button">🗑</button>';
+
+    } else if ($donor == 'eshop' &&  $donationType == 'fixed') {
+
+        $fixedValue = '1.00';
+
+        $headHtml .= '<span>Μέσω αυτής της αγοράς, θα προσφέρουμε '.$fixedValue.$currency_symbol.' για να υποστηρίξουμε έναν μη κερδοσκοπικό οργανισμό</span>';
+        $html .= '<input type="hidden" data-value="'.$fixedValue.'" data-label="'.$fixedValue.'" />';
+
+    } else if ($donor == 'eshop' &&  $donationType == 'percentage') {
+
+        $percent = '15';
+        $cart = WC()->cart;
+        $subtotal = $cart->get_subtotal();
+        $percentValue = $subtotal * $percent / 100;
+
+        $headHtml .= '<span>Θα δωρίσουμε το '.$percent.'% της παραγγελίας σας σε φιλανθρωπικό οργανισμό</span>';
+        $html .= '<input type="hidden" data-value="'.$percentValue.'" data-label="'.$percentValue.'" />';
+
+    }
+
+/**
+ * =======================
+ * Dummpy Values  - End
+ * =======================
+ */
+
+
 ?>
 
 <div class="donation-checkout-widget youbehero-donation-widget">
@@ -23,8 +77,7 @@ $amounts = array_values($youbehero_data['donation_settings']['fixed_amounts']);
 
         <div class="donation-box-container">
             <div class="donation-header">
-                <span>Θα θέλατε να κάνετε μια δωρεά;</span>
-                <span class="donation-amount-pill">0,00<?php echo $currency_symbol;?></span>
+                <?php echo $headHtml; ?>
             </div>
 
             <div class="custom-dropdown">
@@ -48,15 +101,7 @@ $amounts = array_values($youbehero_data['donation_settings']['fixed_amounts']);
         </div>
 
         <div class="donation-buttons donation-amounts">
-            <?php //render_donation_buttons($amounts);
-            foreach ($amounts as $amount) {
-                $amount_cents = (int)$amount * 100;
-                ?>
-                <button class="radio-button" data-value="<?php echo $amount_cents; ?>" data-label="<?php echo $amount; ?>" ><?php echo $amount; ?></button>
-                <?php
-            }
-            ?>
-            <button class="delete-button">🗑</button>
+            <?php echo $html; ?>
         </div>
         <input name="donation_cause" id="donation-cause" type="hidden"/>
         <input name="donation_amount" id="donation-amount" type="hidden"/>
